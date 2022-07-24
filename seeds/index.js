ebe9436b564500b8b08ba07b755c65cc0948aeb4
Darpandeep Kaur <darpansandhu195@gmail.com>
2022-07-24 02:55:37 -0700

    Basic layout and styles using bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-const campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    // useCreateIndex: true,
+    useCreateIndex: true,
     useUnifiedTopology: true
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "coneection error:"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
@@ -22,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++ ){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus itaque architecto ullam laborum porro quod voluptas cupiditate. Dolor eius ab inventore earum voluptatibus voluptas quasi repudiandae, incidunt fuga adipisci fugiat?',
+            price
         })
         await camp.save();
     }
