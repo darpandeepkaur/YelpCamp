e361e4175f8376cc5220a0e167868846a59f535a
Darpandeep Kaur <darpansandhu195@gmail.com>
2022-08-12 02:34:23 -0700

    Adding basic Authorization, controllers and star ratings

--- routes/users.js
+++ routes/users.js
@@ -2,41 +2,16 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(async (req, res) => {
-    try{
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-        const { username, email, password } = req.body;
-        const user = new User({ username, email });
-        const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YelpCamp');
-        res.redirect('/campgrounds');
-
-    } catch (e) {
-
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
-
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome Back' );
-    res.redirect('/campgrounds');
-})
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Goodbye');
-    res.redirect('/campgrounds');
-})
+router.get('/logout', users.logout);
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
+            author: '62f18bf662741bb0791b2581',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
