a580980a28ec3d05d4bc54a0509a9cbf836b0b90
Darpandeep Kaur <darpansandhu195@gmail.com>
2022-08-13 00:50:48 -0700

    Uploading image to cloudinary and storing in mongo

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const {isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer  = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
 const campgrounds = require('../controllers/campgrounds')
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampgrounds));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampgrounds));
 
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
